d8796963feef3d9d394093036fb76c85111de5c5
Dennis Bappert <hello@dbpprt.ai>
2023-09-10 08:29:19 +0200

    getting config in place

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-from argparse import Namespace, ArgumentParser
+from argparse import ArgumentParser
 
-def main(params: Namespace) -> None:
-    print(params)
+from apscheduler.executors.pool import ThreadPoolExecutor
+from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.schedulers.background import BackgroundScheduler
+from omegaconf import DictConfig, ListConfig, OmegaConf
+from pytz import utc
 
-def parse_args() -> Namespace:
+jobstores = {
+    "default": MemoryJobStore(),
+}
+executors = {
+    "default": ThreadPoolExecutor(20),
+}
+job_defaults = {"coalesce": False, "max_instances": 3}
+
+
+def main(config: ListConfig | DictConfig) -> None:
+    _scheduler = BackgroundScheduler(
+        jobstores=jobstores,
+        executors=executors,
+        job_defaults=job_defaults,
+        timezone=utc,
+    )
+
+    print(config)
+
+
+def parse_config() -> ListConfig | DictConfig:
     parser = ArgumentParser()
     parser.add_argument(
-        '--input', type=str, default='data/input.txt', help='input file')
-    parser.add_argument(
-        '--output', type=str, default='data/output.txt', help='output file')
-    return parser.parse_args()
+        "--config_file",
+        type=str,
+        default="config/config.yaml",
+        help="configuration file",
+    )
+    params = parser.parse_args()
+
+    config = OmegaConf.load(params.config_file)
+    cli_config = OmegaConf.from_cli()
+    config = OmegaConf.merge(config, cli_config)
+
+    return config
+
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    config = parse_config()
+    main(config)
