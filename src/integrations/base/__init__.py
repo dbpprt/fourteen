b61252de961e1c494a8f0fc4d6dc22f90c4eac09
Dennis Bappert <hello@dbpprt.ai>
2023-09-17 10:21:55 +0200

    ruff seems to be not happy with __init__.py

--- src/integrations/base/__init__.py
+++ src/integrations/base/__init__.py
@@ -1,2 +1,3 @@
-from src.integrations.base.integration import BaseIntegration, Integration  # noqa: F401
+# isort: skip_file
 from src.integrations.base.telegram_handler import TelegramHandler  # noqa: F401
+from src.integrations.base.integration import BaseIntegration, Integration  # noqa: F401
